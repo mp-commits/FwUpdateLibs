fa3180751e56a2a8caf41ba07600754a2746476a
Mikael Penttinen <mikael.penttinen3@gmail.com>
2025-07-04 15:25:41 +0300

    Add updateserver sources

---
+++ updateserver/include/updateserver/protocol.h
@@ -0,0 +1,65 @@
+/* MIT License
+ * 
+ * Copyright (c) 2025 Mikael Penttinen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * protocol.h
+ *
+ * @brief Protocol definitions for update server
+*/
+
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*----------------------------------------------------------------------------*/
+/* PUBLIC MACRO DEFINITIONS                                                   */
+/*----------------------------------------------------------------------------*/
+
+#define PROTOCOL_SID_PING               (0x01U)
+#define PROTOCOL_SID_READ_DATA_BY_ID    (0x02U)
+#define PROTOCOL_SID_WRITE_DATA_BY_ID   (0x03U)
+#define PROTOCOL_SID_PUT_METADATA       (0x10U)
+#define PROTOCOL_SID_PUT_FRAGMENT       (0x11U)
+
+#define PROTOCOL_ACK_OK                     (0x00U)
+#define PROTOCOL_NACK_REQUEST_OUT_OF_RANGE  (0xE0U)
+#define PROTOCOL_NACK_INVALID_REQUEST       (0xE1U)
+#define PROTOCOL_NACK_BUSY_REPEAT_REQUEST   (0xE2U)
+#define PROTOCOL_NACK_REQUEST_FAILED        (0xE3U)
+#define PROTOCOL_NACK_INTERNAL_ERROR        (0xE4U)
+
+#define PROTOCOL_DATA_ID_FIRMWARE_VERSION   (0x01U)
+#define PROTOCOL_DATA_ID_FIRMWARE_TYPE      (0x02U)
+#define PROTOCOL_DATA_ID_FIRMWARE_NAME      (0x03U)
+#define PROTOCOL_DATA_ID_FIRMWARE_UPDATE    (0x10U)
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+/* EoF protocol.h */
+
+#endif /* PROTOCOL_H_ */
---
+++ updateserver/include/updateserver/server.h
@@ -0,0 +1,154 @@
+/* MIT License
+ * 
+ * Copyright (c) 2025 Mikael Penttinen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * server.h
+ *
+ * @brief Interface for update server
+*/
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*----------------------------------------------------------------------------*/
+/* INCLUDE DIRECTIVES                                                         */
+/*----------------------------------------------------------------------------*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+/*----------------------------------------------------------------------------*/
+/* PUBLIC TYPE DEFINITIONS                                                    */
+/*----------------------------------------------------------------------------*/
+
+/** Read data using an identifier
+ * 
+ * @param id Data ID
+ * @param out Read buffer
+ * @param maxSize Maximum read size
+ * @param readSize Actual read size
+ * 
+ * @return Result code
+ */
+typedef uint8_t (*ReadDataById_t)(
+    uint8_t id, 
+    uint8_t* out, 
+    size_t maxSize, 
+    size_t* readSize
+);
+
+/** Write data using an identifier
+ * 
+ * @param id Data ID
+ * @param in Write buffer
+ * @param size Write size
+ * 
+ * @return Result code
+ */
+typedef uint8_t (*WriteDataById_t)(
+    uint8_t id, 
+    const uint8_t* in, 
+    size_t size
+);
+
+/** Put new metadata
+ * 
+ * @param data Data buffer
+ * @param size Data size
+ * 
+ * @return Result code
+ */
+typedef uint8_t (*PutMetadata_t)(
+    const uint8_t* data, 
+    size_t size
+);
+
+/** Put new fragment
+ * 
+ * @param data Data buffer
+ * @param size Data size
+ * 
+ * @return Result code
+ */
+typedef uint8_t (*PutFragment_t)(
+    const uint8_t* data, 
+    size_t size
+);
+
+typedef struct 
+{
+    ReadDataById_t  ReadDid;
+    WriteDataById_t WriteDid;
+    PutMetadata_t   PutMetadata;
+    PutFragment_t   PutFragment;
+} UpdateServer_t;
+
+/*----------------------------------------------------------------------------*/
+/* PUBLIC FUNCTION DECLARATIONS                                               */
+/*----------------------------------------------------------------------------*/
+
+/** Initialize server instance
+ * 
+ * @param server Server instance
+ * @param readDid Read service
+ * @param writeDid Write service
+ * @param putMetadata Put new metadata service
+ * @param putFragment Put new fragment service
+ * 
+ * @return Initialization successful
+ */
+extern bool US_InitServer(
+    UpdateServer_t* server,
+    ReadDataById_t  readDid,
+    WriteDataById_t writeDid,
+    PutMetadata_t   putMetadata,
+    PutFragment_t   putFragment);
+
+/** Process incoming update server request
+ * 
+ * @param request Request buffer
+ * @param requestLength Request length
+ * @param response Response buffer
+ * @param maxResponseLength Maximum number of bytes to put to response buffer
+ * 
+ * @return Number of bytes in response
+ */
+extern size_t US_ProcessRequest(
+    const UpdateServer_t* server,
+    const uint8_t* request,
+    size_t requestLength,
+    uint8_t* response,
+    size_t maxResponseLength);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+/* EoF server.h */
+
+#endif /* SERVER_H_ */
